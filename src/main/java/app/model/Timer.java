3be53c8865e08fabd1f3b3c90387b1016d9b32c1
Diana <diashka.klen2000@mail.ru>
2023-04-16 20:56:11 +0300

    Задание выполнено

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,13 @@
 package app.model;
 
-public class Timer {
+import org.springframework.stereotype.Component;
 
+@Component("timerBean")
+public class Timer {
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
     }
+
 }
